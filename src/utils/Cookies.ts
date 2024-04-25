439e10dc34911105ebf104be87e448a8d8842603
Hieu Pham <trunghieupham03@gmail.com>
2024-04-25 23:13:26 +0700

    feat: encode/ decode token

--- src/utils/Cookies.ts
+++ src/utils/Cookies.ts
@@ -1,37 +1,18 @@
 import Constants from '@/utils/Constants';
 import { getCookie, setCookie } from 'cookies-next';
-// import * as crypto from 'crypto';
-
-// const algorithm = 'aes-256-cbc';
-// const key = crypto.randomBytes(32);
-// const iv = crypto.randomBytes(16);
-
-// function encryptData(text: string): string {
-//   const cipher = crypto.createCipheriv(algorithm, key, iv);
-//   let encrypted = cipher.update(text, 'utf-8', 'hex');
-//   encrypted += cipher.final('hex');
-//   return encrypted;
-// }
-
-// function decryptData(text: string): string {
-//   const decipher = crypto.createDecipheriv(algorithm, key, iv);
-//   let decrypted = decipher.update(text, 'hex', 'utf-8');
-//   decrypted += decipher.final('utf-8');
-//   return decrypted;
-// }
 
 export function setAppCookie(
   name: string,
   value: any,
   encrypt: boolean = false,
 ) {
-  const data = encrypt ? value : value;
+  const data = encrypt ? Buffer.from(value).toString('base64') : value;
   setCookie(name, data, { maxAge: 60 * 60 * 24 });
 }
 
 export function getAppCookie(name: string, decrypt: boolean = false): string {
   const data = getCookie(name) ?? '';
-  return decrypt ? data : data;
+  return decrypt ? Buffer.from(data, 'base64').toString('ascii') : data;
 }
 
 export function setAppToken(access_token: string, refresh_token: string) {
